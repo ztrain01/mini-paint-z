eec37d6da8ccd0ed28078b4d05cbe80226b4c1d7
Zander Allen <ztrain01@gmail.com>
2015-02-05 20:42:03 -0700

    double click removes white box

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
     $('.box').on('click', function() {
       $(this).addClass('white');
   })
-
+    $('.box').on('dblclick', function() {
+      $(this).removeClass('white');
+  })
     
     
 })
